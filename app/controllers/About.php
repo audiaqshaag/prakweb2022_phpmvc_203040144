5bd5136d7f5a8d3d17b59d4e11ef69b0d385e815
audi <audiaqsha578@gmail.com>
2022-01-26 09:22:45 +0700

    Pertemuan 4, Video 4, Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,15 @@
+<?php
+
+class About
+{
+    public function index($nama = 'Audi Aqsha', $pekerjaan = 'Pejudi'){
+        echo "Hallo, nama saya $nama, saya adalah seorang $pekerjaan";
+
+    }
+   
+
+    public function page()
+    {
+        echo 'About/page';
+    }
+}
